d3a680ac23f52ed56b6c510b31b2fb6ecfa769c3
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-09-02 09:32:37 +0800

    fix: typo with title

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       supportedLocales: const [
         Locale('en','SG'),
       ],
-      title: 'Flutter Demo',
+      title: 'DateCalc',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
